7fa20b134ef47e5d3f7cf2a5c244531742a91485
Arash <arash@Arashs-MacBook-Pro.local>
2017-11-29 19:44:38 -0800

    first version of client

--- src/client.go
+++ src/client.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 )
 
-// type Test struct {
-// 	custom_func LambdaFunc
-// }
+ type GenericTypeV struct {
+ 		integer int
+		boolean bool
+}
+
 
 // func CustomFunc (a GenericTypeA, s string, value GenericTypeV) GenericTypeA {
 // 	if (value == 100) {
@@ -19,7 +21,27 @@ import (
 // }
 
 func main() {
-	// var d DictInterface
-	// var real_d DictInterface
-	// real_d = d.Empty
+	 var d DictInterface
+	 var pair_value GenericTypeV
+	 var real_d DictInterface
+	 var key = ""
+	 var key1 = ""
+	 // create a new empty dictionary
+	 real_d = d.Empty()
+
+	 for i := 1; i < 500; i++ {
+		key := key + "a" + "a"
+		integer.pair_value = i
+		if i%2 == 0 {
+			boolean.pair_value = true
+		} else {
+			boolean.pair_value = false
+		}
+		real_d.Insert(key, pair_value)
+	 }
+	 for i:=1; i<333; i++ {
+		 key1 := key1 + "a" + "a" + "a"
+		 real_d.Lookup(key1)
+	 }
+
 }
