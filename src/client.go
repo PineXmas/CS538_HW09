737883f942ffd4e1c32099be341b058c44871e6f
Arash <arash@Arashs-MacBook-Pro.local>
2017-11-29 21:27:09 -0800

    third version

--- src/client.go
+++ src/client.go
@@ -1,8 +1,7 @@
 // Arash is responsible for this file
 
 package main
-
-import "fmt"
+//import "fmt"
 
  type PairType struct {
  		integer int
@@ -12,12 +11,15 @@ type aType struct {
 	a int
 }
 
-func CustomFunc (a aType, s string, value PairType) aType {
-	var count int
-	count = a
- if boolean.value == true{
-	 count = count + 1
- }
+type MyLambdaFunc func() aType
+
+func (m MyLambdaFunc) CustomFunc (a1 aType, s string, value PairType) aType{
+	var count aType
+	count.a = a1.a
+ if value.boolean == true {
+	count.a = count.a + 1
+}
+ return count
 }
 
 func main() {
@@ -27,23 +29,28 @@ func main() {
 	 var real_d DictInterface
 	 var key = ""
 	 var key1 = ""
+	 var m MyLambdaFunc
+
 	 // create a new empty dictionary
 	 real_d = d.Empty()
 
 	 for i := 1; i < 500; i++ {
 		key := key + "a" + "a"
-		integer.pair_value = i
+		pair_value.integer = i
 		if i%2 == 0 {
-			boolean.pair_value = true
+			pair_value.boolean = true
 		} else {
-			boolean.pair_value = false
+			pair_value.boolean = false
 		}
 		real_d.Insert(key, pair_value)
+		//fmt.Println(real_d.Lookup(key))
 	 }
+	 //key1 = "bb"
+	 //fmt.Println(real_d.Lookup(key1))
 	 for i:=1; i<333; i++ {
 		 key1 := key1 + "a" + "a" + "a"
 		 real_d.Lookup(key1)
 	 }
-	 a.a_fold = 0
-	 real_d.Fold(a_fold, CustomFunc) aType
+	 a_fold.a = 0
+	 real_d.Fold(a_fold, m.CustomFunc)
 }
