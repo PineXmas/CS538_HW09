10bc2e6958bbf3eec5d1754b997b6b137897a57e
Unknown <pinexmas@gmail.com>
2017-12-01 02:19:10 -0800

    add some commens to beautify the codes

--- src/client.go
+++ src/client.go
@@ -34,12 +34,19 @@ func main() {
 	var a_fold aType
 	var real_d DictInterface
 
-	// create a new empty dictionary
+	// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	// |PART 1|
+	// Create a new empty dictionary
+	// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	real_d = d.Empty()
 
-	// For each i, 1 <= i < 500, insert a (key,value) pair constructed as follows. The key should be a string of 2i copies
-	// of the character ’a’. The corresponding value should be the pair (i, b) where b is true if i is even and false
+	// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	// |PART 2|
+	// For each i, 1 <= i < 500, insert a (key,value) pair constructed as follows.
+	// The key should be a string of 2i copies of the character ’a’.
+	// The corresponding value should be the pair (i, b) where b is true if i is even and false
 	// otherwise.
+	// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	var key = ""
 	var err error
 	for i := 1; i < 500; i++ {
@@ -51,30 +58,37 @@ func main() {
 		} else {
 			pair_value.boolean = false
 		}
-		// Test to see if the key is in the dictionary yet, we expect it is not before insert
+
+		// Test: to see if the key is in the dictionary yet, we expect it is not before insert
 		if real_d.Lookup(key).isFound {
 			err = errors.New("Error: Somehow this key:  " + key + "  already existed in the dictionary")
 			log.Fatal(err)
 		}
 		real_d.Insert(key, pair_value)
-		// Test to see if the key is in the dictionary yet, we expect it is after insert
+
+		// Test: to see if the key is in the dictionary yet, we expect it is after insert
 		if !real_d.Lookup(key).isFound {
 			err = errors.New("Error: Cannot insert " + key + " into the dictionary properly")
 			log.Fatal(err)
 		}
 	}
 
+	// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	// |PART 3|
 	// For each i, 1 <= i < 333, lookup the key consisting of 3i copies of the character ’a’
+	// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	key = ""
 	for i := 1; i < 333; i++ {
 		key = key + "a" + "a" + "a"
-		// Test if the key is really 3i copies of a
+
+		// Test: if the key is really 3i copies of a
 		if len(key)%3 != 0 {
 			err = errors.New("Error: This key:  " + key + " is not 3i copies of a")
 			log.Fatal(err)
 		}
+
+		// Test: if the key exists, we expect the value of that key to be a pair
 		if real_d.Lookup(key).isFound {
-			// If the key exists, we expect the value of that key to be a pair
 			switch real_d.Lookup(key).value.(type) {
 			case PairType:
 				continue
@@ -83,13 +97,25 @@ func main() {
 				log.Fatal(err)
 			}
 		}
+
+		// Test: if the key is 2k copies of a, the key should be found
+		if len(key)%2 == 0 {
+			if !real_d.Lookup(key).isFound {
+				err = errors.New("Error: Expected key:  " + key + " is to be found")
+				log.Fatal(err)
+			}
+		}
 	}
 
-	//  Use a fold to compute the total number of entries in the dictionary containing a value whose boolean component
-	// is true.
+	// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	// |PART 4|
+	// Use a fold to compute the total number of entries in the dictionary containing a value
+	// whose boolean component is true.
+	// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	a_fold.a = 0
 	var total = real_d.Fold(a_fold, CustomFunc)
-	// Test if the total number of entries is 249
+
+	// Test: if the total number of entries is 249
 	if total.(aType).a != 249 {
 		err = errors.New(" Error: Unexpected result for Fold function ")
 		log.Fatal(err)
