24b0db0630cb73dd31b32c4c8a9dd6c40bb578f7
Minh Vu <minhvu@pdx.edu>
2017-11-30 16:05:24 -0800

    did some cleanup

--- src/client.go
+++ src/client.go
@@ -1,46 +1,44 @@
 // Arash is responsible for this file
 
 package main
+
 import "fmt"
 
- type PairType struct {
- 		integer int
-		boolean bool
+// PairType : the values to be used in the dictionary
+type PairType struct {
+	integer int
+	boolean bool
 }
+
 type aType struct {
 	a int
 }
 
 //type MyLambdaFunc func() aType
-
-func CustomFunc(a1 GenericTypeA, s string, value GenericTypeV) GenericTypeA{
+func CustomFunc(a1 GenericTypeA, s string, value GenericTypeV) GenericTypeA {
 	var count aType
 	count.a = a1.(aType).a
- if value.(PairType).boolean == true {
-	count.a = count.a + 1
-}
+	if value.(PairType).boolean == true {
+		count.a = count.a + 1
+	}
 
- return count
-}
-
-// TestType hahaha
-type TestType struct {
-	a int
+	return count
 }
 
 func main() {
-	 var d Dictionary
-	 var pair_value PairType
-	 var a_fold aType
-	 var real_d DictInterface
-	 var key = ""
-	 var key1 = ""
-	 //var m MyLambdaFunc
+	var d Dictionary
+	var pair_value PairType
+	var a_fold aType
+	var real_d DictInterface
 
-	 // create a new empty dictionary
-	 real_d = d.Empty()
+	// create a new empty dictionary
+	real_d = d.Empty()
 
-	 for i := 1; i < 500; i++ {
+	// For each i, 1 <= i < 500, insert a (key,value) pair constructed as follows. The key should be a string of 2i copies
+	// of the character ’a’. The corresponding value should be the pair (i, b) where b is true if i is even and false
+	// otherwise.
+	var key = ""
+	for i := 1; i < 500; i++ {
 		key = key + "a" + "a"
 
 		pair_value.integer = i
@@ -51,23 +49,27 @@ func main() {
 		}
 		real_d.Insert(key, pair_value)
 
-	 }
+	}
 
-	 //key1 = "bb"
-	 //fmt.Println(real_d.Lookup(key1))
-	 for i:=1; i<333; i++ {
-		 key1 = key1 + "a" + "a" + "a"
-		 fmt.Print(len(key1))
-		 fmt.Print(" = ")
-		 fmt.Print(real_d.Lookup(key1).isFound)
+	// For each i, 1 <= i < 333, lookup the key consisting of 3i copies of the character ’a’
+	key = ""
+	for i := 1; i < 333; i++ {
+		key = key + "a" + "a" + "a"
+		fmt.Print(len(key))
+		fmt.Print(" = ")
+		fmt.Print(real_d.Lookup(key).isFound)
 
-		 if  real_d.Lookup(key1).isFound == true {
-		 	fmt.Print(real_d.Lookup(key1).value)
-		 }
+		if real_d.Lookup(key).isFound == true {
+			fmt.Print("   ")
+			fmt.Print(real_d.Lookup(key).value)
+		}
 
-		 fmt.Println(" ")
-	 }
-	 a_fold.a = 0
+		fmt.Println(" ")
+	}
 
-	 fmt.Println(real_d.Fold(a_fold, CustomFunc))
+	//  Use a fold to compute the total number of entries in the dictionary containing a value whose boolean component
+	// is true.
+	a_fold.a = 0
+	fmt.Print("Total # of entries: ")
+	fmt.Println(real_d.Fold(a_fold, CustomFunc))
 }
--- src/imp_hashed_table.go
+++ src/imp_hashed_table.go
@@ -2,15 +2,18 @@
 
 package main
 
+// Dictionary a dictionary implemented using hashtable structure
 type Dictionary struct {
 	hashtable map[string]GenericTypeV
 }
 
+// Empty return an empty dictionary
 func (d Dictionary) Empty() DictInterface {
 	d.hashtable = make(map[string]GenericTypeV)
 	return d
 }
 
+// Lookup lookup & return the given string in this dictionary
 func (d Dictionary) Lookup(s string) LookupResult {
 	value, ok := d.hashtable[s]
 	if ok {
@@ -25,10 +28,12 @@ func (d Dictionary) Lookup(s string) LookupResult {
 	return error
 }
 
+// Insert insert the given pair of string and value into this dictionary
 func (d Dictionary) Insert(s string, value GenericTypeV) {
 	d.hashtable[s] = value
 }
 
+// Fold perform the given function on all the elements in this dictionary
 func (d Dictionary) Fold(a GenericTypeA, client_func LambdaFunc) GenericTypeA {
 	if len(d.hashtable) == 0 {
 		return a
