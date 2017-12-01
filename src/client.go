dffb584b9bbc3cccb3a4681d4f85e16175a53167
Minh Vu <minhvu@pdx.edu>
2017-12-01 00:07:00 -0800

    added some tests

--- src/client.go
+++ src/client.go
@@ -2,7 +2,10 @@
 
 package main
 
-import "fmt"
+import (
+	"errors"
+	"log"
+)
 
 // PairType : the values to be used in the dictionary
 type PairType struct {
@@ -14,7 +17,7 @@ type aType struct {
 	a int
 }
 
-//type MyLambdaFunc func() aType
+// CustomFunc : use to support the Fold function
 func CustomFunc(a1 GenericTypeA, s string, value GenericTypeV) GenericTypeA {
 	var count aType
 	count.a = a1.(aType).a
@@ -38,6 +41,7 @@ func main() {
 	// of the character ’a’. The corresponding value should be the pair (i, b) where b is true if i is even and false
 	// otherwise.
 	var key = ""
+	var err error
 	for i := 1; i < 500; i++ {
 		key = key + "a" + "a"
 
@@ -47,29 +51,47 @@ func main() {
 		} else {
 			pair_value.boolean = false
 		}
+		// Test to see if the key is in the dictionary yet, we expect it is not before insert
+		if real_d.Lookup(key).isFound {
+			err = errors.New("Error: Somehow this key:  " + key + "  already existed in the dictionary")
+			log.Fatal(err)
+		}
 		real_d.Insert(key, pair_value)
-
+		// Test to see if the key is in the dictionary yet, we expect it is after insert
+		if !real_d.Lookup(key).isFound {
+			err = errors.New("Error: Cannot insert " + key + " into the dictionary properly")
+			log.Fatal(err)
+		}
 	}
 
 	// For each i, 1 <= i < 333, lookup the key consisting of 3i copies of the character ’a’
 	key = ""
 	for i := 1; i < 333; i++ {
 		key = key + "a" + "a" + "a"
-		fmt.Print(len(key))
-		fmt.Print(" = ")
-		fmt.Print(real_d.Lookup(key).isFound)
-
-		if real_d.Lookup(key).isFound == true {
-			fmt.Print("   ")
-			fmt.Print(real_d.Lookup(key).value)
+		// Test if the key is really 3i copies of a
+		if len(key)%3 != 0 {
+			err = errors.New("Error: This key:  " + key + " is not 3i copies of a")
+			log.Fatal(err)
 		}
-
-		fmt.Println(" ")
+		if real_d.Lookup(key).isFound {
+			// If the key exists, we expect the value of that key to be a pair
+			switch real_d.Lookup(key).value.(type) {
+			case PairType:
+				continue
+			default:
+				err = errors.New("Error: Expected this lookup value to be a pair")
+				log.Fatal(err)
+			}
+		}
 	}
 
 	//  Use a fold to compute the total number of entries in the dictionary containing a value whose boolean component
 	// is true.
 	a_fold.a = 0
-	fmt.Print("Total # of entries: ")
-	fmt.Println(real_d.Fold(a_fold, CustomFunc))
+	var total = real_d.Fold(a_fold, CustomFunc)
+	// Test if the total number of entries is 249
+	if total.(aType).a != 249 {
+		err = errors.New(" Error: Unexpected result for Fold function ")
+		log.Fatal(err)
+	}
 }
